75ded3cbcf383177c98f1805573e0601868ff597
ARYOSVALLDO CLEEF de SOUZA <x10487855485@tcu.gov.br>
2019-04-29 16:06:47 -0300

    start application

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MenuSuperior from './components/menu-superior/MenuSuperior';
+import Resumo from './components/resumo/Resumo';
+
+function App() {
+  return (
+    <div>
+      <MenuSuperior/>
+      <Resumo/>
+    </div>
+  );
+}
+
+export default App;
